998aaa5d6802d085a60a2d675eba455295b4ee10
Ethan Ryan <ethanryan@gmail.com>
2017-03-03 19:15:23 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,132 @@
 # Add  code here!
+prime_number_array = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997]
+#array of prime numbers up to 1,000
+#need to put this array inside method, because #prime? can only take one argument, number
+
+
+#via: http://math.stackexchange.com/questions/783393/a-quick-way-to-determine-whether-a-number-is-prime-by-hand
+#
+# To determine if n is prime:
+#
+# Find the biggest perfect square k2 ≤ n.
+# Write out all the primes less than or equal to k.
+# Test if n is divisible by each of said primes on your list.
+# If n is divisible by any of the primes, n is not prime.
+# If n is divisible by none of the primes, n is prime.
+
+#note:
+#an integer is a whole number (not a fraction) that can be positive, negative, or zero
+#a prime number is a positive integer which has no factors other than 1 and itself
+
+# def small_prime?(integer)
+#   prime_number_array = [2, 3, 5, 7, 11] #array of prime numbers up to 1,000
+#   #number = number / number #test number by its square root
+#   prime_number_array.each do |prime|
+#     if (integer % prime == 0) && (integer != prime) #the modulo operation finds the remainder after division of one number by another... 0 means there are no remainders
+#       return false
+#     else
+#       return true
+#     end #end if...else statement
+#   end #end for each loop
+# end #end method
+
+
+#### i cheated... used Math and got this from here:::
+#### http://stackoverflow.com/questions/11762043/how-can-i-test-if-a-value-is-a-prime-number-in-ruby-both-the-easy-and-the-hard
+
+def prime?(integer)
+  #prime_number_array = [1763, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997] #array of prime numbers up to 1,000
+
+  if integer <= 1 # everything below 2 is not a prime
+    return false
+  elsif
+    Math.sqrt(integer).to_i.downto(2).each {|i| return false if integer % i == 0} # squareroot of each integer (to_i: to integer) downto 2; 2 is the smallest prime so it's downto(2)... and then if the integer passed in is divisible by a prime with no remainders, it means they are not a prime, so false is returned.
+    ### if ((integer % prime == 0) && (integer != prime)) # the modulo operation finds the remainder after division of one number by another... 0 means there are no remainders
+  return true
+end #end if..else method
+end #end method
+
+
+
+#   prime_number_array.each do |prime|
+#     if ((integer % prime == 0) && (integer != prime)) || ((integer < 2) && (integer.even?) && (integer != 2))#the modulo operation finds the remainder after division of one number by another... 0 means there are no remainders
+#       return false
+#     else
+#       return true
+#     end #end if...else statement
+#   end #end for each loop
+# end #end method
+
+
+
+# def prime?(integer)
+#   prime_number_array = [1763, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997] #array of prime numbers up to 1,000
+#   prime_number_array.each do |prime|
+#     if ((integer % prime == 0) && (integer != prime)) || ((integer < 2) && (integer.even?) && (integer != 2))#the modulo operation finds the remainder after division of one number by another... 0 means there are no remainders
+#       return false
+#     else
+#       return true
+#     end #end if...else statement
+#   end #end for each loop
+# end #end method
+
+
+
+# def prime?(integer)
+#   #prime_number_array = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997]
+#   prime_number_array = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997]
+#   #array of prime numbers up to 1,000
+#   #number = number / number #test number by its square root
+#   if integer <= 1 #all prime numbers are greater than 1
+#     false
+#   elsif (integer.even?) && (integer != 2) # all prime numbers are odd except for 2
+#     false
+#   elsif (integer % 3 == 0) && (integer != 3) # prime numbers are not divisible by 3, except for 3
+#     false
+#   elsif (integer % 5 == 0) && (integer != 5) # prime numbers are not divisible by 5, except for 5
+#     false
+#   elsif (integer % 7 == 0) && (integer != 7) # prime numbers are not divisible by 7, except for 7
+#     false
+#   elsif prime_number_array.include?(integer) #first 1,000 prime numbers are in array above
+#     true
+#   else
+#     true
+#   end #end if..else statement
+# end #end method
+
+
+
+# def prime?(integer)
+#   prime_number_array = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997]
+#   #array of prime numbers up to 1,000
+#   #number = number / number #test number by its square root
+#
+#   integer_is_prime = [] #empty array to hold integer if it is a prime number
+#
+#   prime_number_array.each do |prime|
+#     #integer / prime
+#     if integer / prime == 0 #the modulo operation finds the remainder after division of one number by another... 0 means there are no remainders
+#       integer_is_prime << integer
+#     end #end if statement
+#  end #end loop
+#
+#   if integer_is_prime.any?
+#     true
+#   else
+#     false
+#   end #end if..else statement
+# end #end method
+
+
+#### below function passes some tests, but not if integer is greater than 1,000
+
+# def prime?(integer)
+#   prime_number_array = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997]
+#   #array of prime numbers up to 1,000
+#   #number = number / number #test number by its square root
+#   if prime_number_array.include?(integer)
+#     true
+#   else
+#     false
+#   end #end if..else statement
+# end #end method
